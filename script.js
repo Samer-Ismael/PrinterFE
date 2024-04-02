79013b0d5957ea39830c849df6d529ece5b8c079
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-04-02 06:25:14 +0200

    Making style

--- script.js
+++ script.js
@@ -1,59 +1,122 @@
 function homePrinter() {
-    sendRequest('home');
+    sendGCodeCommand('G28');
 }
 
 function emergencyStop() {
-    sendRequest('emergency_stop');
+    sendGCodeCommand('M112');
 }
 
 function preheatPLA() {
-    sendRequest('preheat_pla');
+    sendGCodeCommand('M140 S60');
+    sendGCodeCommand('M104 S210');
 }
 
 function preheatABS() {
-    sendRequest('preheat_abs');
+    sendGCodeCommand('M140 S90');
+    sendGCodeCommand('M104 S240');
+}
+
+function coolDown() {
+    sendGCodeCommand('M140 S0');
+    sendGCodeCommand('M104 S0');
 }
 
 function sendCommand(event) {
     event.preventDefault();
     var command = document.getElementById('command').value;
-    sendRequest(command);
+
+    if (command.toLowerCase() === 'clear') {
+        document.getElementById('response').innerText = '';
+        document.getElementById('command').value = '';
+        return;
+    }else {
+        document.getElementById('response').value = 'command sent: ' + command;
+        sendGCodeCommand(command);
+    }
 }
 
-function getTemperatures() {
-    fetch('your_endpoint_url')
-        .then(response => response.json())
-        .then(data => {
-            // Update Extruder temperature
-            document.getElementById('extruderTemp').textContent = data.extruderTemp ? data.extruderTemp + '°C' : '0°C';
-            // Update Heater Bed temperature
-            document.getElementById('heaterBedTemp').textContent = data.heaterBedTemp ? data.heaterBedTemp + '°C' : '0°C';
-        })
-        .catch(error => console.error('Error:', error));
-}
+function getTemperatures(includeMonitors) {
+    const FLUIDD_SERVER_URL = "http://192.168.0.71";
+    const TEMPERATURE_STORE_ENDPOINT = "/server/temperature_store";
 
-function sendRequest(command) {
-    fetch('your_endpoint_url', {
-        method: 'POST',
-        body: JSON.stringify({ command: command }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+    const url = FLUIDD_SERVER_URL + TEMPERATURE_STORE_ENDPOINT;
+    const params = { include_monitors: includeMonitors };
+
+    fetch(url, { method: 'GET', params: params })
         .then(response => response.json())
         .then(data => {
-            if (command === 'get_temperatures') {
-                displayTemperatures(data);
-            } else {
-                displayResponse(data);
+            const recentTemperatures = {};
+            for (const sensorName in data.result) {
+                const sensorData = data.result[sensorName];
+                const temperatures = sensorData.temperatures;
+                if (temperatures && temperatures.length > 0) {
+                    const recentTemperature = temperatures[temperatures.length - 1];
+                    recentTemperatures[sensorName] = recentTemperature;
+                }
             }
+
+            // Update Extruder temperature
+            const extruderTemp = recentTemperatures['extruder'] || '0.00';
+            document.getElementById('extruderTemp').textContent = extruderTemp + '°C';
+
+            // Update Heater Bed temperature
+            const heaterBedTemp = recentTemperatures['heater_bed'] || '0.00';
+            document.getElementById('heaterBedTemp').textContent = heaterBedTemp + '°C';
         })
         .catch(error => console.error('Error:', error));
 }
 
-function displayResponse(data) {
-    document.getElementById('response').innerText = data.response;
+
+
+
+function sendGCodeCommand(gCodeCommand) {
+    const url = `http://192.168.0.71:7125/printer/gcode/script?script=${encodeURIComponent(gCodeCommand)}`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(responseBody => {
+            const responseData = JSON.parse(responseBody);
+            displayResponse(gCodeCommand, responseData.result);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
 }
+
+document.getElementById('command').addEventListener('keydown', function(event) {
+    // Check if the key pressed is Enter (key code 13)
+    if (event.keyCode === 13) {
+        // Prevent the default behavior of the Enter key (form submission)
+        event.preventDefault();
+        // Get the value of the input element
+        const gCodeCommand = this.value.trim();
+        // Check if the command is not empty
+        if (gCodeCommand) {
+            // Call the sendGCodeCommand function with the entered command
+            sendGCodeCommand(gCodeCommand);
+            // Clear the input field
+            this.value = '';
+        }
+    }
+});
+
+
+function displayResponse(command, responseData) {
+    const responseElement = document.getElementById('response');
+    // Construct the string representation of the command and response
+    const message = `Command sent: ${command}\nResponse: ${JSON.stringify(responseData)}`;
+    // Append the message to the response element
+    responseElement.innerText += message + '\n';
+    responseElement.scrollTop = responseElement.scrollHeight;
+
+}
+
 
 function displayTemperatures(data) {
     var temperatureResponse = '';
@@ -79,10 +142,17 @@ document.addEventListener('DOMContentLoaded', function() {
     getFiles();
 });
 
-function getFiles() {
-    fetch('your_files_endpoint_url')
-        .then(response => response.json())
+function getFiles(rootFolder) {
+    const url = `http://192.168.0.71:7125/server/files/list?root=${rootFolder}`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
+            console.log('Files:', data); // Log the response data
             displayFiles(data);
         })
         .catch(error => console.error('Error:', error));
@@ -94,43 +164,80 @@ function displayFiles(files) {
 
     files.forEach(file => {
         const listItem = document.createElement('li');
-        listItem.textContent = file.name;
+        listItem.textContent = file.path;
 
         // Add click event listener to list item
         listItem.addEventListener('click', function() {
             // Show the print button
             document.getElementById('printFileButton').style.display = 'inline';
             // Store the selected file name as a data attribute
-            document.getElementById('printFileButton').setAttribute('data-file-name', file.name);
+            document.getElementById('printFileButton').setAttribute('data-file-path', file.path);
         });
 
         fileListElement.appendChild(listItem);
     });
 }
 
-function printSelectedFile() {
-    // Get the selected file name from the data attribute
-    const fileName = document.getElementById('printFileButton').getAttribute('data-file-name');
-    if (fileName) {
-        // Send request to API to print the selected file
-        sendPrintRequest(fileName);
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const rootFolder = {
+        name: "gcodes",
+        path: "/home/samer/printer_data/gcodes",
+        permissions: "rw"
+    };
+    console.log('Fetching files from:', rootFolder.path); // Log the root folder path
+    getFiles(rootFolder);
+});
 
-function sendPrintRequest(fileName) {
-    // Send request to API to print the file
-    fetch('your_print_endpoint_url', {
-        method: 'POST',
-        body: JSON.stringify({ fileName: fileName }),
-        headers: {
-            'Content-Type': 'application/json'
+document.addEventListener('DOMContentLoaded', function() {
+    const uploadForm = document.getElementById('uploadForm');
+    uploadForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+        if (!file) {
+            alert('Please select a file.');
+            return;
         }
+
+        uploadFile(file);
+    });
+});
+
+function uploadFile(file) {
+    const url = 'http://192.168.0.71:7125/server/files/upload';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('root', 'gcodes'); // Specify the root location for uploading
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
     })
         .then(response => response.json())
         .then(data => {
-            // Handle response from the API
-            console.log('File printing request sent:', data);
-            // Optionally, display a message to the user indicating that the file is being printed
+            console.log('File uploaded:', data);
+            //alert('File uploaded successfully.');
+            displayResponse('File uploaded', data);
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => {
+            console.error('Error uploading file:', error);
+            alert('Error uploading file. Please try again.');
+        });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const uploadButton = document.querySelector('.upload-button');
+    const clearButton = document.querySelector('.clear-button');
+    const fileInput = document.getElementById('fileInput');
+
+    uploadButton.addEventListener('click', function() {
+        // Your upload logic here
+        // This event listener handles the upload button click
+    });
+
+    clearButton.addEventListener('click', function() {
+        // Clear the selected file
+        fileInput.value = '';
+    });
+});
